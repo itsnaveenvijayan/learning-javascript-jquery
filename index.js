86268f2a4a0b39778c8413f385db38d132c77106
itsnaveenvijayan <itsnaveenvijayan@gmail.com>
2019-12-14 17:56:49 +0530

    4 files modified

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// Import stylesheets
 import './style.css';
+import $ from "jquery";
 
 const loginForm = document.getElementById('loginform');
 loginForm.onsubmit = (event)=>{
@@ -26,13 +26,62 @@ loginForm.onsubmit = (event)=>{
   xhr.send(JSON.stringify(formData)); 
 }
 
-var showSuccess = (xhr) => {
-   document.getElementById('error').innerHTML= '';
-   document.getElementById("success").innerHTML = xhr.responseText;
+const logoutForm = document.getElementById('logoutform');
+logoutForm.onsubmit = (event)=>{
+  event.preventDefault();
+  $('#loginform').show()
+  $('#logoutform').hide()
+  $('#message').empty();
+}
+
+var showSuccess = (xhr) => {   
+   $('#logoutform').show();
+   $('#loginform').hide()
+   $('#message').html(xhr.responseText).css('color','green');
+   listUsers();
 }
 
 var showError = (xhr) => {
-  document.getElementById("success").innerHTML = '';
-  document.getElementById('error').innerHTML= xhr.responseText;
+  $('#logoutform').hide();
+  $('#loginform').show();
+  $('#message').html(xhr.responseText).css('color','red');
 }
+
+var listUsers = () => {
+  $.ajax({
+    type: 'GET',
+    url: 'https://reqres.in/api/users',
+    contentType: 'application/json',
+    success: (data) => {
+      
+      let _html = "<tr>";
+      $.each(data.data[0], (val) => {
+        _html += `<th>`+val+`</th>`;
+      })
+      _html += '</tr>'
+
+      console.log(data.data);
+      $.each(data.data, (i, val) => {
+        _html += "<tr>"
+        console.log(val);
+        $.each(val, (j, val2) => {
+          if (val2.toString().indexOf('.jpg') == -1)
+          {
+              _html += `<td>`+val2+`</td>`;
+          }
+          else{
+              _html += `<td><img src="`+val2+`" alt="Smiley face" height="42" width="42"></td>`;
+          }        
+        });
+        _html += '</tr>'
+      });
+      
+      $('#users-table').html(_html)
+    },
+    error: (xhr) => {
+      alert(xhr.responseText);
+    }
+  });
+}
+
 
